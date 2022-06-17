a47795b694a48e5023f554e29acac40009779591
Manoj <manojsoft11@gmail.com>
2022-06-18 04:18:02 +0530

    Broken Unit Test(:...

--- src/SimpleAPI/Controllers/WeatherForecastController.cs
+++ src/SimpleAPI/Controllers/WeatherForecastController.cs
@@ -51,7 +51,7 @@ namespace SimpleAPI.Controllers
         public ActionResult <string> Get(int id)
         {
             var rng = new Random();
-            return "Manoj Project";
+            return "Manoj Projec";
         }
     }
 }
